3cac4fea5f43f1093807d3ce70431f791bc88354
Ivo Woltring <ivo@ivonet.nl>
2021-03-17 14:54:50 +0100

    added out to clean

--- clean.sh
+++ clean.sh
@@ -5,6 +5,7 @@ echo "Cleaning project...."
 rm -rf __pycache__ 2>/dev/null
 rm -rf dist 2>/dev/null
 rm -rf build 2>/dev/null
+rm -rf out 2>/dev/null
 rm -rf generated 2>/dev/null
 mkdir generated 2>/dev/null
 

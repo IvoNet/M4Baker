ee658e04096a670b89c5f1c2da7cdcade31a6252
Ivo Woltring <ivo@ivonet.nl>
2021-03-21 13:26:46 +0100

    improving on release build

--- build.sh
+++ build.sh
@@ -6,31 +6,46 @@ if [ -z "${VIRTUAL_ENV}" ]; then
   exit 1
 fi
 
-if [ "$1" == "clean" ]; then
+if [ "$1" == "release" ] && [ ! -z "$(git diff --stat)" ]; then
+  echo "Can not release as the project is 'dirty'."
+  echo "Please make sure to commit everything before running a release build"
+  exit 1
+fi
+
+has_create_dmg="$(which create-has_create_dmg)"
+if [ -z "$has_create_dmg" ]; then
+  echo "create-dmg is not installed."
+  echo "if you want to make a release please make sure to install it first!"
+fi
+
+if [ "$1" == "clean" ] || [ "$1" == "release" ]; then
   echo "Cleaning..."
   if [ -d "./dist" ]; then
     rm -rf "./dist" "./build" 2>/dev/null
   fi
 fi
+
 echo "Versioning..."
 python ./version.py
 
 echo "Building..."
-
-echo "Building the m4baker.app"
 ./images.sh
 python -OO -m PyInstaller --osx-bundle-identifier nl.ivonet.m4baker --noconsole -y m4baker.spec
 
+echo "Tagging..."
 ./tag.sh
 
-check="$(which create-dmg)"
-if [ -z "$check" ]; then
-  echo "Not createing DMG as create-dmg is not installed."
-else
-  cd dist
-  create-dmg --overwrite --dmg-title M4Baker M4Baker.app
-  if [ -f "M4Baker 0.0.0.dmg" ]; then
-    version=$(<../VERSION)
-    mv "M4Baker 0.0.0.dmg" "M4Baker_${version}".dmg
+if [ "$1" == "release" ]; then
+  echo "Packaging..."
+  if [ -z "$has_create_dmg" ]; then
+    echo "Not creating DMG release as create-dmg is not installed."
+  else
+    cd ./dist
+    create-dmg --overwrite --dmg-title M4Baker M4Baker.app
+    if [ -f "M4Baker 0.0.0.dmg" ]; then
+      version=$(<../VERSION)
+      mv "M4Baker 0.0.0.dmg" "M4Baker_${version}.dmg"
+      sha512sum "M4Baker_${version}.dmg" >"M4Baker_${version}.dmg.sha512"
+    fi
   fi
 fi
